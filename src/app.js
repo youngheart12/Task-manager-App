d078efb6d3b08d686f91bfd30b0ec3f0ba0c4141
youngheart12 <manishkumar199222@gmail.com>
2019-07-20 14:40:29 +0530

    fixing the cors issue

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors=require('cors')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 app.use(userRouter)
